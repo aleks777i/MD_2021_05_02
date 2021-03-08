f063f6fd8d35d39face41a5a96aca658719d565f
aleks777i <77581142+aleks777i@users.noreply.github.com>
2021-03-08 19:26:48 +0200

    Update 5.1.py

--- 5.1.py
+++ 5.1.py
@@ -8,21 +8,21 @@ class Phone:
 
 
 class Samsung(Phone):
-    def __init__(self):
-        super().__init__()
-        self.brand = "Samsung"
-        self.os = "Android"
+    brand = "Samsung"
+    os = "Android"
 
 
 class Apple(Phone):
-    def __init__(self):
-        super().__init__()
-        self.brand = "Apple"
-        self.os = "ios"
+    brand = "Apple"
+    os = "ios"
 
 
 iphone12 = Apple()
 print(iphone12.brand, iphone12.os, iphone12.number)
 s7 = Samsung()
 print(s7.brand, s7.os, s7.number)
+s9 = Samsung()
+print(s9.brand, s9.number)
+
+
 
